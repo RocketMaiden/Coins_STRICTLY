4258d24fa1dc1ce4ee6d1f71563df0d21c741a06
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-04-05 15:00:02 +0300

    string user input goes in char array

--- Coins_STRICTLY/Program.cs
+++ Coins_STRICTLY/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿char [] sequence = new char[70];
+for (int i = 0; i < sequence.Length; i++)
+{
+    sequence[i] = 'X';
+}
+
+string userInput = "HHHTTHHHHHTT";
+sequence = userInput.ToUpper().ToCharArray();
+
+
